e2752b0e0abf9e12708e1ce1d442427045d09220
rid <ridamnagar11@gmail.com>
2019-01-04 20:40:00 +0530

    Name : Ridam updated to coolrid

--- database/mongodb-connect.js
+++ database/mongodb-connect.js
@@ -1,6 +1,6 @@
 //const MongoClient=require('mongodb').MongoClient;
 const {MongoClient,ObjectID}=require('mongodb');
-MongoClient.connect('mongodb://localhost:27017/prochat',(error,client)=>{
+MongoClient.connect('mongodb://localhost:27017/prochat',{ useNewUrlParser: true },(error,client)=>{
     if(error){
         return console.log('Unable to connect to the database');
     }
@@ -16,14 +16,39 @@ MongoClient.connect('mongodb://localhost:27017/prochat',(error,client)=>{
 //         }
 //         console.log(JSON.stringify(result.ops,undefined,2));
 //     })
-    db.collection('users').find({
-        Name:'karan Nagar'
-    }).toArray().then((docs)=>{
-      console.log(docs,undefined,2);
-    },(err)=>{
-         if(err){
-             return console.log('unable to find ',err);
-         }
+//     db.collection('users').find({
+//         Name:'karan Nagar'
+//     }).toArray().then((docs)=>{
+//       console.log(docs,undefined,2);
+//     },(err)=>{
+//          if(err){
+//              return console.log('unable to find ',err);
+//          }
+//     })
+    // db.collection('users').deleteMany({Name:'Ridam Nagar'}).then((result)=>{
+    //             console.log(result.opts,undefined,2);
+    // },(error)=>{
+    //     if(error){
+    //         return console.log('not found');
+    //     }
+    // })
+    db.collection('users').findOneAndDelete({Name:'karan Nagar'},(error,result)=>{
+        if(error){
+             return console.log('not found',error);
+        }
+        console.log(result.opts,undefined,2);
+    }) 
+    db.collection('users').findOneAndUpdate({
+        Name:'Ridam Nagar'
+    },{
+        $rename:{
+            Name:'Username'
+        }
+    },{
+        returnOriginal:false
+    }).then((result)=>{
+        console.log(result);
     })
+      
     client.close();
 })
