fb43c79b451ef859434ce48d93763d599245b644
rid <ridamnagar11@gmail.com>
2019-02-25 23:29:55 +0530

    logout added

--- server.js
+++ server.js
@@ -18,6 +18,9 @@ app.use(express.static(__dirname + '/public'));
 
 app.use(bodyParser.urlencoded({extended:false}))
 
+
+
+
 app.get('/',(req,res)=>{
 
 
@@ -42,45 +45,14 @@ app.post('/',(req,res)=>{
     var user=new users(body);
     console.log(body);
     user.save().then((user)=>{
-        console.log(os.hostname());
-        let transporter = nodemailer.createTransport({
-            host:'mail.traversymedia.com',
-            port: 587,
-            secure: false, // true for 465, false for other ports
-            Oauth: {
-                user: 'ridamnagar11@gmail.com', // generated ethereal user
-                pass: 'g8bhaiya'  // generated ethereal password
-            },
-            tls:{
-              rejectUnauthorized:false
-            }
-          });
-        
-          // setup email data with unicode symbols
-          let mailOptions = {
-              from: '"Nodemailer Contact" <ridamnagar11@gmail.com>', // sender address
-              to: 'rishabhn417@gmail.com', // list of receivers
-              subject: 'Node Contact Request', // Subject line
-              text: 'Hello world?', // plain text body
-             // html: output // html body
-          };
-        
-          // send mail with defined transport object
-          transporter.sendMail(mailOptions, (error, info) => {
-              if (error) {
-                  return console.log(error);
-              }
-              console.log('Message sent: %s', info.messageId);   
-              console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-        
-              res.render('contact', {msg:'Email has been sent'});
-          });
+    
+       
         return user.generateAuthToken();
        
     }).then((token)=>{
-
+               
                res.header('x-auth',token).render('home.hbs',{
-                name:"Hey"+user.name+" you have successfully",
+                name:"Hey "+user.name+" you have successfully",
                 Login:"login",
                 type:"registered"
             });
@@ -230,3 +202,22 @@ app.get('/chat',(req,res)=>{
 app.listen(port,()=>{
     console.log(`server is up on the port ${port}`);
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
