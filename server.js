d90c3216481777830b247150bacb486b6d017581
rid <ridamnagar11@gmail.com>
2019-02-26 20:53:54 +0530

    add connect and disconnect event handelers

--- server.js
+++ server.js
@@ -12,10 +12,26 @@ var {authenticate}=require('./middleware/authenticate.js');
 const os=require('os');
 const port=process.env.PORT || 3000;
 const http=require("http");
+//this is built in module
 const socketIO=require("socket.io");
 
 var server=http.createServer(app);
+//happens at the backside but this time we are using it here.
 var io=socketIO(server);
+//we have access to the server via server variable passed in as argument.
+//socketIO(server) return a websocket server.
+//here we can do what we want in terms emitting or listening to event
+io.on('connection' ,(socket)=>{
+console.log("new user connected");
+socket.on('disconnect',()=>{
+    console.log("user was disconnected");
+})
+});
+//io.on let's you register an event listener,we can listen for specific event and do something when that event happens
+//we will be using one built in event called connection as an argument.This let's you listen for new connection and do something 
+//when that connection comes in.Inorder to do something we provide a callback function which is called with socket argument.
+// 
+
 app.set('view engine','hbs');
 hbs.registerPartials(__dirname + '/views/partials');
 app.use(express.static(__dirname + '/public'));
@@ -25,7 +41,7 @@ app.use(bodyParser.urlencoded({extended:false}))
 
 
 
-app.get('/',(req,res)=>{
+app.get('/registration',(req,res)=>{
 
 
     res.render('registration.hbs',{
@@ -43,7 +59,7 @@ app.get('/',(req,res)=>{
 
 })
 
-app.post('/',(req,res)=>{
+app.post('/registration',(req,res)=>{
       var body=lodash.pick(req.body,['email','password','name'])
      
     var user=new users(body);
@@ -114,9 +130,7 @@ app.post('/login',(req,res)=>{
        console.log(hashedpassword);
        console.log(body.password);
        bcrypt.compare(body.password,hashedpassword,(error,result)=>{
-           console.log("heyyyyyyyyyyyyyyyyyyyyyyyyy");
-           console.log();
-           console.log(error);
+          
            if(result==true){
         
             getToken(userToken);
@@ -169,7 +183,7 @@ app.post('/login',(req,res)=>{
         link3:"link3",
         loggedin:user.email,
         Login:"Logout",
-        name:"Hey"+user.name+" you have successfully",
+        name:"Hey "+user.name+" you have successfully",
         type:"logged in"
     })
       console.log("user:"+user);
@@ -184,7 +198,7 @@ app.delete('/logout',authenticate,(req,res)=>{
      console.log("loaded successfully");
     res.render('home.hbs',{
         Login:"Login",
-        name:"Hey"+user.name+" you have successfully",
+        name:"Hey "+user.name+" you have successfully",
         type:"logged out"
     })
  },(e)=>{
@@ -193,7 +207,7 @@ app.delete('/logout',authenticate,(req,res)=>{
      res.status(400).send(e);
  })
 })
-app.get('/chat',(req,res)=>{
+app.get('/',(req,res)=>{
    // res.send("Home page");
    res.render('home.hbs',{
        title:"ProChat",
@@ -206,7 +220,8 @@ app.get('/chat',(req,res)=>{
 server.listen(port,()=>{
     console.log(`server is up on the port ${port}`);
 })
-
+//earlier i used app.listen() which on backside call server.listen() by passing app as an argument in createServer() method 
+//called by http. 
 
 
 
